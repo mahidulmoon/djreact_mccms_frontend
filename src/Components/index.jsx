243362e2198f8e9b1bdcbcdda5ea31603d454895
mahidulmoon <mahidulmoon@gmail.com>
2021-01-18 18:43:31 +0600

    added footer

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import NavBar from "./NavBar"
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 class MainBody extends Component {
     render() {
         return (
             <React.Fragment>
                 <NavBar />
+                <Footer/>
             </React.Fragment>
         );
     }
