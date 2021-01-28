1b58498ea6e61b8793fa019311ab1e2d8b7fce98
mahidulmoon <mahidulmoon@gmail.com>
2021-01-28 22:37:05 +0600

    complain added in admin dashboard

--- src/Components/Admin/Complains.jsx
+++ src/Components/Admin/Complains.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import {FcAddDatabase} from "react-icons/fc";
+import axios from "axios";
 class Complains extends Component {
+    state = {
+        complains:[],
+        next:'',
+        previous:'',
+        count: 0,
+    }
+    componentDidMount(){
+        axios.get('http://127.0.0.1:8000/api/complain/complains/').then(res => {this.setState({complains: res.data.results,next:res.data.next,previous:res.data.previous,count:res.data.count})}).catch(err => {console.log(err);alert("error to fetch complain")});
+
+    }
+    nextpage = () =>{
+        axios.get(this.state.next).then(res => this.setState({complains:res.data.results,next:res.data.next,previous:res.data.previous}))
+    } 
+    previouspage = () =>{
+        axios.get(this.state.previous).then(res => this.setState({complains:res.data.results,next:res.data.next,previous:res.data.previous}))
+    } 
     render() {
         return (
             <div>
@@ -15,35 +32,39 @@ class Complains extends Component {
                         </div>
                     </div>
                     <div className="row">
+                        {this.state.complains.map(complain =>
                         <div className="col-lg-6">
-                        <div className="tm-activity-block">
-                            <div className="tm-activity-img-container">
-                            <img src="favicon.ico" alt="logo" className="tm-activity-img" />
-                            </div>
-                            <div className="tm-activity-block-text">
-                            <h3 className="tm-text-blue">Lorem ipsum dolor sit amet</h3>
-                            <p>
-                                Orci varius natoque penatibus et magnis dis parturient montes,
-                                nascetur ridiculus mus. Sed eu turpis nec sem lacinia
-                                condimentum et a orci.
-                            </p>
-                            <code><FcAddDatabase/> Mark as Agree</code>
-                            </div>
+                            <div className="tm-activity-block">
+                                <div className="tm-activity-img-container">
+                                    <img src={complain.image_field} height="60" alt="logo" className="tm-activity-img" />
+                                    <br/>
+                                    <p className="text-danger bg-light"> {complain.status}</p>
+                                    <p>{complain.created_at.slice(11,19)} <br/>{complain.created_at.slice(0,10)}</p>
+                                </div>
+                                <div className="tm-activity-block-text">
+                                <h3 className="tm-text-blue">{complain.complain_subject}</h3>
+                                <p>
+                                    {complain.complain}
+                                </p>
+                                
+                                <code className="col-lg-6"><FcAddDatabase/> ({complain.avg_rating}) Agree</code>
+                                    
+                                
+                                </div>
+                            </div>   
                         </div>
-                        </div>
+                        )}
                         
                     </div>
                     </div>
-                    <nav className="col-lg-6" aria-label="Page navigation example">
+                    <nav className="container" aria-label="Page navigation example">
                             <ul className="pagination justify-content-end">
-                                <li className="page-item disabled">
-                                <a className="page-link" href="/feeds" tabindex="-1" aria-disabled="true">Previous</a>
+                                <li className="page-item ">
+                                <button className="page-link" tabindex="-1" onClick={this.previouspage}>Previous</button>
                                 </li>
-                                <li className="page-item"><a className="page-link" href="/feeds">1</a></li>
-                                <li className="page-item"><a className="page-link" href="/feeds">2</a></li>
-                                <li className="page-item"><a className="page-link" href="/feeds">3</a></li>
+                                
                                 <li className="page-item">
-                                <a className="page-link" href="/feeds">Next</a>
+                                <button className="page-link" onClick={this.nextpage}>Next</button>
                                 </li>
                             </ul>
                         </nav>
