9dde87ab2a199351ae0492689ff22293ac74ac70
mahidulmoon <mahidulmoon@gmail.com>
2021-02-03 12:37:41 +0600

    userinfor static on complain page

--- src/Components/ComplainForm/Form.jsx
+++ src/Components/ComplainForm/Form.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 class ComplainForm extends Component {
-    
+    state = {
+        userinfo:[],
+    }
+    componentWillMount(){
+        axios.get(`http://127.0.0.1:8000/api/user/registeruser/${localStorage.getItem('userid')}/`).then(res => this.setState({userinfo:res.data})).catch(err => {console.log(err);alert("error to fetch userinfo")})
+
+    }
     render() {
         if(!localStorage.getItem('token')){
             return <Redirect path="/login" />
@@ -13,14 +20,15 @@ class ComplainForm extends Component {
                     <br/>
                     <div class="complainPage">
                         <h2> Complain Page </h2>
-                        <form action="" method="post" enctype="multipart/form-data">
+                        {this.state.userinfo.user && 
+                            <form method="post" enctype="multipart/form-data">
                             
                             <label for="name">Your Name (Username)</label>
-                            <input type="text" name="name" id="name" required="required" autocomplete="off"  value="{{ user.username }}"/>
+                            <input type="text" name="name" id="name" required="required" autocomplete="off"  value={this.state.userinfo.user.username} disabled/>
                             <label for="email">Your Email </label>
-                            <input type="email" name="email" id="email"  autocomplete="off"   value="{{ user.email }}" />
+                            <input type="email" name="email" id="email"  autocomplete="off"   value={this.state.userinfo.user.email}  disabled/>
                             <label for="phone-number">Your Phone Number </label>
-                            <input type="text" name="phone-number" id="phone-number" required="required" autocomplete="off"   placeholder="Enter your phone number"/>
+                            <input type="text" name="phone-number" id="phone-number" required="required" autocomplete="off"   value={this.state.userinfo.phone} disabled/>
                             <label for="area">Your Area Name or Address </label>
                             <input type="text" name="area" id="area" required="required" autocomplete="off"   placeholder="Complaint area name"/>
                             <label for="pincode">Area Postal Code </label>
@@ -38,6 +46,7 @@ class ComplainForm extends Component {
                             <input type="submit" value="Submit" required />
                             
                         </form>
+                        }
 
                     </div>
                 </div>
